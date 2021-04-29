b463f46d6ad46fa54ddc925628451d13bed4c75b
khiati-nour <khiatihouda@gmail.com>
2021-04-29 10:52:27 +0200

    reinstall node modules

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,11 +1,11 @@
 import "./style.css";
 
-const About = () => {
+const Contact = () => {
   return (
     <div className="contact">
       <h1>Contact me!</h1>
     
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40166.55499022335!2d3.5880606647110884!3d51.0085797662868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c36d8ff5d3ff61%3A0x40099ab2f4d5c80!2sLaethem-Saint-Martin!5e0!3m2!1sfr!2sbe!4v1618563982570!5m2!1sfr!2sbe"
+      <iframe title="My location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40166.55499022335!2d3.5880606647110884!3d51.0085797662868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c36d8ff5d3ff61%3A0x40099ab2f4d5c80!2sLaethem-Saint-Martin!5e0!3m2!1sfr!2sbe!4v1618563982570!5m2!1sfr!2sbe"
        
         loading="lazy"></iframe>
       <h4>Phone</h4>
@@ -20,4 +20,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Contact;
